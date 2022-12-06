bcb3384d706aae90ac3f8cbb31c2055d243f35b7
Spiro <riccardospironelli@gmail.com>
2022-12-06 17:52:01 +0100

    Switched to an api based application

---
+++ OraiQzer.Api/Repository/Repository/ScheduleRepository.cs
@@ -0,0 +1,40 @@
+﻿using OrariQzer.ApplicationCore.Interfaces.Repository;
+using OrariQzer.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OraiQzer.Api.Extensions;
+
+namespace OrariQzer.Domain.Repository
+{
+    public class ScheduleRepository : IScheduleRepository
+    {
+        private readonly string url;
+        private readonly IConfiguration _configuration;
+
+        public ScheduleRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            url = _configuration.GetConnectionString("DriveSheet")!;
+        }
+
+
+        public async Task<IEnumerable<Schedule>> getSchedule(bool fromToday = false)
+        {
+            using var client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string worksheet = await response.Content.ReadAsStringAsync();
+
+            return worksheet.Split('\n')
+                .Skip(1)
+                .Select(x => x.ToSchedule())
+                .Where(x => !fromToday || x.WeekDay.DayOfYear >= DateTime.Now.DayOfYear);
+
+        }
+
+    }
+}
